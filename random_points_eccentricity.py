0888e5e608cff37698dd6895eb75fe09e90500c9
meginako <mnako19@epoka.edul.al>
2024-06-11 17:15:35 +0200

    more advancements

--- random_points_eccentricity.py
+++ random_points_eccentricity.py
@@ -1,6 +1,7 @@
 import numpy as np
 import random
 from skimage import measure
+import matplotlib.pyplot as plt
 
 # Load the segmented tumor array
 tumor_array = np.load(
@@ -8,24 +9,82 @@ tumor_array = np.load(
 test_mask_argmax = np.argmax(tumor_array, axis=3)
 
 
-# Assuming 'matrix' is your 128x128x128 numpy matrix
+# npy_array = test_mask_argmax[:,:,14]
+def eccentricityRandomPoints(npy_array, nr_rand):
 
-# Find contours of the shape in the matrix
-contours = measure.find_contours(test_mask_argmax, 0.5)
+    # Step 1: Visualize the npy array
+    plt.imshow(npy_array, cmap='gray')
+    plt.title('Original Array')
+    plt.show()
 
-# Randomly select 8 coordinates on the contours
-selected_coords = []
-for contour in contours:
-    num_points = contour.shape[0]
-    if num_points >= 8:
-        selected_indices = random.sample(range(num_points), 8)
-        for index in selected_indices:
-            selected_coords.append(contour[index])
+    # Step 2: Find the contours
+    contours = measure.find_contours(npy_array, level=0.5)
 
-# Convert selected coordinates to integer values
-selected_coords = np.round(selected_coords).astype(int)
+    # Find the largest contour
+    # Check if there is only one contour
+    if len(contours) == 1:
+        largest_contour = contours[0]
+    elif len(contours) == 0:
+        return 0
+    else:
+        # Find the largest contour
+        largest_contour = max(contours, key=len)
 
-# Print the selected coordinates
-for coord in selected_coords:
-    print(coord)
+    # check if the contuour is really small or not
+    if len(largest_contour) < nr_rand:
+        return 0
 
+
+    # Step 3: Plot the contours and highlight the largest contour
+    plt.imshow(npy_array, cmap='gray')
+    for contour in contours:
+        plt.plot(contour[:, 1], contour[:, 0], linewidth=2)
+    plt.plot(largest_contour[:, 1], largest_contour[:, 0], linewidth=2, color='red')
+    plt.title('Contours with Largest Contour Highlighted')
+    plt.show()
+
+    # Step 4: Generate 8 random points on the largest contour
+    random_indices = random.sample(range(len(largest_contour)), nr_rand)
+    random_points = largest_contour[random_indices]
+
+    # Plot the random points on the largest contour
+
+    plt.imshow(npy_array, cmap='gray')
+    for contour in contours:
+        plt.plot(contour[:, 1], contour[:, 0], linewidth=2)
+    plt.plot(largest_contour[:, 1], largest_contour[:, 0], linewidth=2, color='red')
+    plt.scatter(random_points[:, 1], random_points[:, 0], color='blue')
+    plt.title(str(nr_rand)+ 'Random Points on Largest Contour')
+    plt.show()
+
+    # Step 5: Compute the SVD
+    # Center the points by subtracting the mean
+    points_centered = random_points - np.mean(random_points, axis=0)
+    U, S, Vt = np.linalg.svd(points_centered)
+
+    # Step 6: Calculate the eccentricity
+    # Eccentricity = sqrt(1 - (minor_axis / major_axis)^2)
+    eccentricity = np.sqrt(1 - (S[1] / S[0]) ** 2)
+    print("Eccentricity:", eccentricity)
+    return eccentricity
+
+
+# combined_array = np.column_stack((Perimeters, Area, EccentricityPA))
+# Save the combined array to a CSV file
+# np.savetxt('G:/Alban & Megi/BrainSegmentation/dataset/predictedNPYResults/img_111_randomPoints.csv', combined_array, delimiter=',')
+
+'''
+EccentrictiyRandom16Points = []
+EccentrictiyRandom32Points = []
+EccentrictiyRandom64Points = []
+
+for i in range(128):
+    EccentrictiyRandom16Points.append(eccentricityRandomPoints(test_mask_argmax[:, :, i], 16))
+    EccentrictiyRandom32Points.append(eccentricityRandomPoints(test_mask_argmax[:, :, i], 32))
+    EccentrictiyRandom64Points.append(eccentricityRandomPoints(test_mask_argmax[:, :, i], 64))
+
+combined_array = np.column_stack((EccentrictiyRandom16Points, EccentrictiyRandom32Points, EccentrictiyRandom64Points))
+# Save the combined array to a CSV file
+np.savetxt('G:/Alban & Megi/BrainSegmentation/dataset/predictedNPYResults/img_111_randomPoints.csv', combined_array,
+           delimiter=',')
+'''
--- svd.py
+++ svd.py
@@ -4,7 +4,6 @@ import mahotas.demos
 import matplotlib.pyplot as plt
 
 
-
 def eccentricity(array):
     # Compute the covariance matrix
     covariance_matrix = np.cov(array, rowvar=False)
@@ -13,45 +12,56 @@ def eccentricity(array):
     # Calculate the eccentricity
     eccentricity = np.sqrt(np.max(eigenvalues) / np.min(eigenvalues))
     return eccentricity
+
+
 def printSlice(i):
-    array = test_mask_argmax[:,:, i]
+    array = test_mask_argmax[:, :, i]
     plt.imshow(array)  # cmap='gray' for grayscale, use other colormaps for different looks
     plt.colorbar()  # Add colorbar to show intensity scale
     plt.title("Picture of slice " + str(i))
     plt.show()
+
+
 def printAllSlices():
-    for i in range(0,128):
-        array = test_mask_argmax[:,:, i]
+    for i in range(0, 128):
+        array = test_mask_argmax[:, :, i]
         plt.imshow(array)  # cmap='gray' for grayscale, use other colormaps for different looks
         plt.colorbar()  # Add colorbar to show intensity scale
         plt.title("Picture of slice " + str(i))
         plt.show()
 
+
 # Load the segmented tumor array
 tumor_array = np.load(
     "G:/Alban & Megi/BrainSegmentation/dataset/BraTS2020_TrainingData/input_data_128/val/masks/mask_111.npy")
 test_mask_argmax = np.argmax(tumor_array, axis=3)
-tumor_array = tumor_array[0, :, :, :]
-# class 0 has info on first col
-# class 1 has info only when we fix the last two cols [i,:,:]
-# class 2 yellow??
-# class 3 has info only when we fix the last two cols [i,:,:] but too roundy, yellow
 
-#U, S, Vt = np.linalg.svd(tumor_array[:, :, 55])
 SIGMAS = []
 RATIOS = []
+RATIOS_FORMULA = []
 MAHOTAS_ECC = []
-#FORMULA_ECC = []
+SLOTS_OCCUPIED =[]
+# FORMULA_ECC = []
 for i in range(128):
-    array = test_mask_argmax[:,:, i]
+    array = test_mask_argmax[:, :, i]
     MAHOTAS_ECC.append(mahotas.features.eccentricity(array))
-    #FORMULA_ECC.append(eccentricity(array))
+    # FORMULA_ECC.append(eccentricity(array))
     u, s, v = np.linalg.svd(array)
     if sum(s) == 0:
         RATIOS.append(0)
+        RATIOS_FORMULA.append(0)
     else:
         RATIOS.append(s[0] / sum(s))
+        RATIOS_FORMULA.append(np.sqrt(1 - (s[1] / s[0]) ** 2))
+    SLOTS_OCCUPIED.append(np.count_nonzero(s))
     SIGMAS.append(s)
-print(max(RATIOS))
-print(max(MAHOTAS_ECC))
-#print(max(FORMULA_ECC))
+
+
+combined_array = np.column_stack((RATIOS, RATIOS_FORMULA, MAHOTAS_ECC,SLOTS_OCCUPIED))
+# Save the combined array to a CSV file
+np.savetxt('G:/Alban & Megi/BrainSegmentation/dataset/predictedNPYResults/img_111_fullImageSVD.csv', combined_array,
+           delimiter=',')
+
+# print(max(RATIOS))
+# print(max(MAHOTAS_ECC))
+# print(max(FORMULA_ECC))
